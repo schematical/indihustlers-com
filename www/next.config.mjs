da968ca664d391ba1cb55b7c65849b7e3bb2cbcb
Your Name <you@example.com>
2024-03-09 10:08:40 -0600

    Init

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        outputFileTracingIgnores: [
+            // '**canvas**',
+            '**swc/core**'
+        ],
+    },
+};
 
 export default nextConfig;
