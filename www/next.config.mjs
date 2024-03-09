be81abebace9c2f113e65aad6a887da448ad08ae
Your Name <you@example.com>
2024-03-09 10:28:05 -0600

    Init

--- www/next.config.mjs
+++ www/next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
+    assetPrefix: process.env.PUBLIC_ASSET_URL,
+    images: {
+        unoptimized: true,
+    }/*,
     experimental: {
         outputFileTracingExcludes: {
            '*': ['**swc/core**']
         },
-    },
+    },*/
 };
 
 export default nextConfig;
